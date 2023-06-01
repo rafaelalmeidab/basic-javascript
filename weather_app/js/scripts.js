b80e53af59d1f4503111218f5e9c6efda6f8e62e
r.almeida <rafael.almeida@rbmweb.com.br>
2023-06-01 13:30:54 -0300

    update: weather functions

--- weather_app/js/scripts.js
+++ weather_app/js/scripts.js
@@ -2,25 +2,104 @@
 $(function(){
 
 
-// *** APIs ***
-// clima, previsão 12 horas e previsão 5 dias: https://developer.accuweather.com/apis
-// pegar coordenadas geográficas pelo nome da cidade: https://docs.mapbox.com/api/
-// pegar coordenadas do IP: http://www.geoplugin.net
-// gerar gráficos em JS: https://www.highcharts.com/demo
+    // *** APIs ***
+    // clima, previsão 12 horas e previsão 5 dias: https://developer.accuweather.com/apis
+    // pegar coordenadas geográficas pelo nome da cidade: https://docs.mapbox.com/api/
+    // pegar coordenadas do IP: http://www.geoplugin.net
+    // gerar gráficos em JS: https://www.highcharts.com/demo
 
-    $.ajax(function(){
-        url: "http://dataservice.accuweather.com/locations/v1/topcities/50?apikey=%094HcUAvGLZiUCq7QChIDcGqFaHwF3kcG3&language=pt-BR",
-        type: "GET",
-        dataType: "json",
-        success: function(data){
-            console.log(data);
-        },
-        error: function(){
-            console.log("Erro!");
-        }
+    var apiKey        = '4HcUAvGLZiUCq7QChIDcGqFaHwF3kcG3';
+    var latitude      = "-21.60";
+    var longitude     = "-42.70";
+    var weatherObject = {
+        cidade: "",
+        estado: "",
+        pais: "",
+        tempetarura: "",
+        textoClima: "",
+        iconeClima: ""
+    };
 
-    });
+    
+    // virgulo em url enconding => %2C
+    function getCoordenadasIP(){ //http://www.geoplugin.net/json.gp
+        $.ajax({
+            url :"http://www.geoplugin.net/json.gp",
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                if(data.geoplugin_latitude && data.geoplugin_longitude){
+                    getLocalUsuario(data.geoplugin_latitude, data.geoplugin_longitude);
+                }
+            },
+            error: function(){
+                console.log("Erro!");
+                getLocalUsuario(latitude, longitude);
+            }
+        });
+    }
+    
+    function getLocalUsuario(latitude, longitude){
+        $.ajax({
+            url :"http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + apiKey + "&q=" + latitude + "%2C" + longitude,
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                try{
+                    weatherObject.cidade = data.ParentCity.LocalizedName;
+                }
+                catch{
+                    weatherObject.cidade = data.LocalizedName;
+                }
+                weatherObject.estado      = data.AdministrativeArea.LocalizedName; 
+                weatherObject.pais        = data.Country.LocalizedName;
+            
+                
+                var cityKey = data.Key;
+                getTempoAtual(cityKey);
+            },
+            error: function(){
+                console.log("Erro!");
+            }
+        });
+    }
+    
+    function getTempoAtual(cityKey){ //language=pt-BR
+        $.ajax({
+            url :"http://dataservice.accuweather.com/currentconditions/v1/" + cityKey +"?apikey=" + apiKey,
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                weatherObject.tempetarura = data[0].Temperature.Metric;
+                weatherObject.textoClima  = data[0].WeatherText;
+                weatherObject.iconeClima  = "";
 
+                setDadosClima(weatherObject);
+            },
+            error: function(){
+                console.log("Erro!");
+            }
+        });
+    }
 
+    function setDadosClima(dados){
+        dados = { "cidade": "Rio de Janeiro",
+                  "estado": "Rio De Janeiro",
+                  "iconeClima": "",
+                  "pais": "Brazil",
+                  "tempetarura":
+                    {
+                        "Unit":"C",
+                        "Value":"25"
+                    },
+                  "textoClima": "Mostly cloudy"
+        };
 
+        console.log(dados);
+        $("#texto_local").html(dados.cidade + ", " + dados.estado + ", " + dados.pais);
+        $("#texto_clima").html(dados.textoClima);
+        $('#texto_temperatura').html(String(dados.tempetarura.Value) + "&deg;");
+    }
+
+    setDadosClima(''); 
 });
