be23cdda3235d235512c2e9e4cef131bc5ac7375
r.almeida <rafael.almeida@rbmweb.com.br>
2023-06-04 23:13:06 -0300

    update: basicJavaScript ok

--- weather_app/js/scripts.js
+++ weather_app/js/scripts.js
@@ -8,7 +8,8 @@ $(function(){
     // pegar coordenadas do IP: http://www.geoplugin.net
     // gerar gráficos em JS: https://www.highcharts.com/demo
 
-    var apiKey        = '4HcUAvGLZiUCq7QChIDcGqFaHwF3kcG3';
+    var apiKey        = '';
+    var apiKeyMapBox  = '';
     var latitude      = "-21.60";
     var longitude     = "-42.70";
     var weatherObject = {
@@ -20,7 +21,6 @@ $(function(){
         iconeClima: ""
     };
 
-    
     // virgulo em url enconding => %2C
     function getCoordenadasIP(){ //http://www.geoplugin.net/json.gp
         $.ajax({
@@ -33,8 +33,31 @@ $(function(){
                 }
             },
             error: function(){
-                console.log("Erro!");
+                setGerarErro("Erro ao pegar coordenadas do usuário!");
                 getLocalUsuario(latitude, longitude);
+            }
+        });
+    }
+
+    function getCoordenadasPesquisaMapBox(dados){
+        var cidade = encodeURI(dados);
+        $.ajax({
+            url :"https://api.mapbox.com/geocoding/v5/mapbox.places" + cidade + ".json?access_token=" + apiKeyMapBox,
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                try{
+                    var latitude  = data.features[0].geometry.coordinates[0];
+                    var longitude = data.features[0].geometry.coordinates[1]; 
+
+                    getLocalUsuario(latitude, longitude);
+                }
+                catch{
+                    setGerarErro("Erro ao pegar coordenadas do MapBox!");
+                }
+            },
+            error: function(){
+                setGerarErro("Erro ao pegar coordenadas do MapBox!");
             }
         });
     }
@@ -57,14 +80,55 @@ $(function(){
                 
                 var cityKey = data.Key;
                 getTempoAtual(cityKey);
+                getPrevisao5Dias(cityKey);
+                getPrevisaoHoraAHora(cityKey);
             },
             error: function(){
-                console.log("Erro!");
+                setGerarErro("Erro ao pegar local do usuário!");
+            }
+        });
+    }
+
+    function getPrevisao5Dias(cityKey){
+        $.ajax({
+            url :"http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + cityKey + "?apikey=" + apiKey + "&language=pt-br&metric=true",
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                $("#texto_max_min").html(String(data.DailyForecasts[0].Temperature.Minimum.Value) + "&deg / " + String(data.DailyForecasts[0].Temperature.Maximum.Value) + "&deg");
+                
+                setPrevisao5Dias(data.DailyForecasts);
+            },
+            error: function(){
+                setGerarErro("Erro ao pegar previsão 5 dias!");
+            }
+        });
+    }
+
+    function getPrevisaoHoraAHora(cityKey){
+        $.ajax({
+            url :"http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/" + cityKey + "?apikey=" + apiKey + "&language=pt-br&metric=true",
+            type: "GET",
+            dataType: "json",
+            success: function(data){
+                var horarios = [];
+                var tempetaruras = []; 
+                for(var i=0; i<data.length; i++){
+                    var hora = new Date(data[i].DateTime).getHours();
+                    horarios.push(String(hora) + "h");
+                    tempetaruras.push(data[i].Temperature.Value);
+                }
+
+                setGerarGrafico(horarios, tempetaruras);
+                $(".refresh-loader").fadeOut();
+            },
+            error: function(){
+                setGerarErro("Erro ao pegar previsão hora a hora!");
             }
         });
     }
     
-    function getTempoAtual(cityKey){ //language=pt-BR
+    function getTempoAtual(cityKey){
         $.ajax({
             url :"http://dataservice.accuweather.com/currentconditions/v1/" + cityKey +"?apikey=" + apiKey,
             type: "GET",
@@ -78,7 +142,7 @@ $(function(){
                 setDadosClima(weatherObject);
             },
             error: function(){
-                console.log("Erro!");
+                setGerarErro("Erro ao pegar tempo atual!");
             }
         });
     }
@@ -90,6 +154,109 @@ $(function(){
         $('#icone_clima').css('background-image', "url('" + weatherObject.iconeClima + "')");
     }
 
+    function setGerarErro(mensagem){
+        if(!mensagem){
+            mensagem = "Erro na solicitação!";
+        }
+        else{
+            $(".refresh-loader").hide();
+            $("#aviso-erro").text(mensagem);
+            $("#aviso-erro").slideDown();
+            window.setTimeout(function(){
+                $("#aviso-erro").slideUp();
+            }, 4000);
+
+
+        }
+
+    }
+
+    function setGerarGrafico(horarios, tempetaruras){
+        Highcharts.chart('hourly_chart', {
+            chart: {
+                type: 'line'
+            },
+            title: {
+                text: 'Temperatura Hora a Hora'
+            },
+            xAxis: {
+                categories: horarios
+            },
+            yAxis: {
+                title: {
+                    text: 'Temperatura (°C)'
+                }
+            },
+            plotOptions: {
+                line: {
+                    dataLabels: {
+                        enabled: false
+                    },
+                    enableMouseTracking: false
+                }
+            },
+            series: [{
+                showInLegend: false,
+                data: tempetaruras
+            }]
+        });
+    }
+
+    function setPrevisao5Dias(dados){
+        $("#info_5dias").html("");
+        var arrayDiasSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+
+        for(var i=0; i<dados.length; i++){
+            var diaSemana         = new Date(dados[i].Date);
+            diaSemana             = arrayDiasSemana[diaSemana.getDay()];
+            var iconNumber        = dados[i].Day.Icon <= 9 ? "0" + String(dados[i].Day.Icon) : dados[i].Day.Icon;
+            var iconeClima        = "https://developer.accuweather.com/sites/default/files/" + iconNumber + "-s.png";
+            var tempetaruraMinima = String(dados[i].Temperature.Minimum.Value);
+            var tempetaruraMaxima = String(dados[i].Temperature.Maximum.Value);
+
+            var elementoHTML = "<div class='day col'>";
+            elementoHTML    += "<div class='day_inner'>";
+            elementoHTML    += "<div class='dayname'>";
+            elementoHTML    += diaSemana;
+            elementoHTML    += "</div>";
+            elementoHTML    += '<div style="background-image: url(\'' + iconeClima + '\')" class="daily_weather_icon"></div>';
+            elementoHTML    += "<div class='max_min_temp'>";
+            elementoHTML    += tempetaruraMinima + "&deg; / " + tempetaruraMaxima +"&deg";
+            elementoHTML    += "</div>";
+            elementoHTML    += "</div>";
+            elementoHTML    += "</div>";
+
+            $("#info_5dias").append(elementoHTML);
+            elementoHTML = '';
+        }
+    }
+
     getCoordenadasIP();
-    // setDadosClima(''); 
-});
+
+    $("#search-button").on('keyup', function(e){
+        if(e.which == 13){
+            $(".refresh-loader").show();
+            console.log("enter");
+            var local = $("input#local").val();
+            if(local){
+                getCoordenadasPesquisaMapBox(local);
+            }
+            else{
+                alert("Local inválido!"); 
+                $(".refresh-loader").fadeOut();
+            }
+        }
+    });
+
+    $("#search-button").click(function(){
+        var local = $("input#local").val();
+        if(local){
+            $(".refresh-loader").show();
+            getCoordenadasPesquisaMapBox(local);
+        }
+        else{
+            alert("Local inválido!");   
+            $(".refresh-loader").fadeOut();
+        }
+    });
+})
